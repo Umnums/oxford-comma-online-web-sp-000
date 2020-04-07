44fb00183740eee33584f3000401386939f1f0d7
Joseph Kao <josephtkao@gmail.com>
2020-04-07 18:53:38 +0000

    Automatically backed up by Learn

--- lib/oxford_comma.rb
+++ lib/oxford_comma.rb
@@ -1,3 +1,13 @@
 def oxford_comma(array)
-
+  i = 0
+  string = ""
+  if array.length == 1
+    array[0]
+  else
+    for i < array.length - 2
+      string << array[i]
+      string << ", "
+    end
+  end
+  string << ", and #{array[-1]}"
 end
