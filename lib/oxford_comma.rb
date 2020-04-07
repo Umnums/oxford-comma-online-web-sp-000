4ec32b7a07829388c63c0137fad202f347297348
Joseph Kao <josephtkao@gmail.com>
2020-04-07 18:56:25 +0000

    Automatically backed up by Learn

--- lib/oxford_comma.rb
+++ lib/oxford_comma.rb
@@ -3,6 +3,8 @@ def oxford_comma(array)
   string = ""
   if array.length == 1
     return array[0]
+  elsif array.length == 2
+    return "#{array[0]} and #{array[1]}"
   else
     while i <= array.length - 2
       string << array[i]
